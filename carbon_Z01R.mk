3627c4255f1d389d88b0c03222376f993ac5cdc5
Sandeep Sethi <adeveloper79@gmail.com>
2020-04-17 14:47:10 +0000

    Carbon-ify
    Signed-off-by: Sandeep Sethi <adeveloper79@gmail.com>

--- carbon_Z01R.mk
+++ carbon_Z01R.mk
@@ -23,7 +23,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o.mk)
 $(call inherit-product, device/asus/Z01R/device.mk)
 
 # Inherit some common Bliss stuff.
-$(call inherit-product, vendor/bliss/config/common_full_phone.mk)
+$(call inherit-product, vendor/carbon/config/common.mk)
 
 # Inherit PixelGApps
 $(call inherit-product-if-exists, vendor/gapps/gapps.mk)
@@ -32,7 +32,7 @@ PRODUCT_BRAND := asus
 PRODUCT_DEVICE := Z01R
 PRODUCT_MANUFACTURER := asus
 PRODUCT_MODEL := Zenfone 5Z
-PRODUCT_NAME := bliss_Z01R
+PRODUCT_NAME := carbon_Z01R
 
 PRODUCT_GMS_CLIENTID_BASE := android-asus
 
